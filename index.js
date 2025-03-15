de91d1f573f8954275682e3397b7ed7681697661
Excel Ikenna <ikennaexcel@gmail.com>
2025-03-15 12:44:34 +0100

    samesite settings

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"))
 const authenticateToken = (req, res, next) => {
     const token = req.cookies.token; // Get token from cookies
 
-    if (!token) return res.sendStatus(401); // Unauthorized
+    if (!token) return res.sendStatus(401).json({message: 'No token found'}); // Unauthorized
 
     jwt.verify(token, process.env.HASH_SECRET, (err, user) => {
         if (err) return res.sendStatus(403); // Forbidden
@@ -90,8 +90,8 @@ app.post("/login", async (req, res) => {
             if (err) return res.status(500).json({ error: "Token generation failed" });
             res.cookie("token", token, {
               httpOnly: true,
-              secure: true, // Set to false if running on localhost without HTTPS
-              sameSite: "None", // Important for cross-origin cookies
+              secure: false, // Set to false if running on localhost without HTTPS
+              sameSite: "lax", // Important for cross-origin cookies // set to lax because frrontend is not depolyed yet
               maxAge: 1000 * 60 * 60 * 24, // Optional: cookie expiration (1 day)
             }).json(userDoc);
           }
