66796f98c5b3405d7d6c2f655f6a18f06f8a1ce6
Excel Ikenna <ikennaexcel@gmail.com>
2025-05-22 16:25:59 +0100

    uploads issue

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
+// app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // JWT MIDDLEWARE
 const authenticateToken = (req, res, next) => {
